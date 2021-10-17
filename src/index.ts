e04985664c7b7bc6f0de2b45644e7c33b0785495
Roland Doda <rolanddoda2014@gmail.com>
2021-10-17 09:43:49 +0200

    Remove `exec` and `cancel` (keep it simple)

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,14 @@ import axios, { AxiosPromise, AxiosRequestConfig, AxiosInstance, CancelTokenSour
 
 const cancelTokens = new Map()
 
-export function api(url: string, config: AxiosRequestConfig = {}, instance: AxiosInstance) {
+export function api(url: string, config: AxiosRequestConfig = {}, instance: AxiosInstance): AxiosPromise {
   const cancelToken: CancelTokenSource = axios.CancelToken.source()
 
-  return {
-    exec: (): AxiosPromise => {
-      cancelTokens.set(cancelToken.token, cancelToken)
-      return instance(url, { ...config, cancelToken: cancelToken.token }).finally(() =>
-        cancelTokens.delete(cancelToken.token)
-      )
-    },
-    cancel: (message?: string) => cancelToken.cancel(message)
-  }
+  cancelTokens.set(cancelToken.token, cancelToken)
+
+  return instance(url, { ...config, cancelToken: cancelToken.token }).finally(() =>
+    cancelTokens.delete(cancelToken.token)
+  )
 }
 
 export const cancelAllPendingRequests = () => cancelTokens.forEach((cancelToken) => cancelToken.cancel())
