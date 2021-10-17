b9c2c18a23984116447b53c4caf492c818244e2d
Roland Doda <rolanddoda2014@gmail.com>
2021-10-17 10:04:42 +0200

    Export `define` for defining api-s

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,21 @@ export function api(url: string, config: AxiosRequestConfig = {}, instance: Axio
 }
 
 export const cancelAllPendingRequests = () => cancelTokens.forEach((cancelToken) => cancelToken.cancel())
+
+export const define = (instance: AxiosInstance) => ({
+  get: (url: string, config?: AxiosRequestConfig) => api(url, { method: 'GET', ...config }, instance),
+
+  post: (url: string, data: unknown, config?: AxiosRequestConfig) => {
+    return api(url, { method: 'POST', data, ...config }, instance)
+  },
+
+  put: (url: string, data: unknown, config?: AxiosRequestConfig) => {
+    return api(url, { method: 'PUT', data, ...config }, instance)
+  },
+
+  patch: (url: string, data: unknown, config?: AxiosRequestConfig) => {
+    return api(url, { method: 'PATCH', data, ...config }, instance)
+  },
+
+  delete: (url: string, config?: AxiosRequestConfig) => api(url, { method: 'DELETE', ...config }, instance)
+})
