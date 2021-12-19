2b6dd09114743ee9d580b8b474e6e781d8e3f959
Roland Doda <rolanddoda2014@gmail.com>
2021-12-19 20:06:24 +0100

    Delete comments

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import axios, {
-  AxiosInstance,
-  AxiosPromise,
-  AxiosRequestConfig,
-  // AxiosResponse,
-  CancelTokenSource
-} from 'axios'
+import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig, CancelTokenSource } from 'axios'
 
 const cancelTokens = new Map()
 
@@ -22,56 +16,6 @@ function getCancelToken() {
 
   return cancelToken
 }
-
-// function getVars() {
-//   return {
-//     loading: ref(false),
-//     data: shallowRef<any>(),
-//     response: shallowRef<AxiosResponse>()
-//   }
-// }
-
-// function request(baseUrl: string, instance: AxiosInstance, type: 'GET' | 'POST') {
-//   const { loading, data, response } = getVars()
-//   let cancelToken: CancelTokenSource
-//   let requestFinished = false
-//
-//   return {
-//     data,
-//     loading,
-//     response,
-//     abort(msg?: string) {
-//       if (requestFinished || !loading.value) return
-//
-//       cancelToken?.cancel(msg)
-//       loading.value = false
-//       requestFinished = true
-//     },
-//     exec(config: AxiosRequestConfig = {}) {
-//       cancelToken = getCancelToken()
-//       loading.value = true
-//       requestFinished = false
-//
-//       return instance({
-//         ...config,
-//         method: type,
-//         cancelToken: cancelToken.token,
-//         url: joinUrls([baseUrl, config.url || ''])
-//       })
-//         .then((res) => {
-//           data.value = res.data
-//           response.value = res
-//
-//           return res
-//         })
-//         .finally(() => {
-//           cancelTokens.delete(cancelToken)
-//           loading.value = false
-//           requestFinished = true
-//         })
-//     }
-//   }
-// }
 
 export function api(baseUrl: string, instance: AxiosInstance) {
   return {
